9ef3008825113ad8e8a212eb3463700c99214236
Yimy.Romero <yimy.romero80@gmail.com>
2025-09-17 23:20:53 -0400

    (fix) Fix typo in the errorHanlder function.

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,7 +1,7 @@
 const { logEvents } = require('./logger');
 
 const errorHandler = (err, req, res, next) => {
-    logEvents(`${err.name}: ${err.message}\t${req.method}\t${req.url}\t${req.headers.origin}, 'errorLog.log`);
+    logEvents(`${err.name}: ${err.message}\t${req.method}\t${req.url}\t${req.headers.origin}`, 'errorLog.log');
     console.log(err.stack);
 
     const status = res.statusCode ? res.statusCode : 500 // server error
