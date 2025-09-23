195abb2361732328a8cc33b96b5122158372532e
Yimy.Romero <yimy.romero80@gmail.com>
2025-09-22 21:22:35 -0400

    (feat) Create connectionconfig.

--- server.js
+++ server.js
@@ -7,9 +7,14 @@ const errorHandler = require('./middleware/errorHandler');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const corsOptions = require('./config/corsOptions');
+const connectDB = require('./config/dbConn');
+const mongoose = require('mongoose');
+const { logEvents } = require('./middleware/logger');
 const PORT = process.env.PORT || 3500;
 
 console.log(process.env.NODE_ENV);
+
+connectDB();
 
 app.use(logger); // middleware
 
@@ -36,4 +41,12 @@ app.all('/*splat', (req, res) => {
 
 app.use(errorHandler);
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+})
+
+mongoose.connection.on('error', err => {
+    console.log(err);
+    logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log');
+})
