74f0a5ebeaefd9d36fec15a87f1bdc8c7cd188e0
Yimy.Romero <yimy.romero80@gmail.com>
2026-01-03 19:17:23 -0500

    (update) Set front end url to run in dev and production.

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,6 @@
-const allowedOrigins = ["https://romerolibrary.onrender.com"];
+const allowedOrigins =
+	process.env.NODE_ENV === "production"
+		? ["https://romerolibrary.onrender.com"]
+		: ["http://localhost:5173", "http://localhost:3000"];
 
 module.exports = allowedOrigins;
